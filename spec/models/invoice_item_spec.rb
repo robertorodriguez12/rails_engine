c8f7377da0a2bc70bfa2f55c6adacb00eaf97533
Hashim Gari <hashmaster3k@gmail.com>
2020-10-18 10:00:57 -0600

    Setup InvoiceItems Resource

---
+++ spec/models/invoice_item_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe InvoiceItem, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :quantity }
+    it { should validate_presence_of :unit_price }
+  end
+end
