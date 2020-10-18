9eee36c3ad85d5bd6bc1f95430fec08cfdf2cc46
Hashim Gari <hashmaster3k@gmail.com>
2020-10-18 10:27:23 -0600

    Update relationships for all models

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -4,4 +4,12 @@ RSpec.describe Invoice, type: :model do
   describe 'validations' do
     it { should validate_presence_of :status }
   end
+
+  describe 'relationships' do
+    it { should belong_to :customer }
+    it { should belong_to :merchant }
+    it { should have_many :invoice_items }
+    it { should have_many(:items).through(:invoice_items) }
+    it { should have_many :transactions }
+  end
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe Transaction, type: :model do
     it { should validate_presence_of :credit_card_number }
     it { should validate_presence_of :result }
   end
+
+  describe 'relationships' do
+    it { should belong_to :invoice }
+  end
 end
