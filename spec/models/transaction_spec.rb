46b12db2c47a37e8b2b6e812b20a892d38075419
Hashim Gari <hashmaster3k@gmail.com>
2020-10-18 10:04:41 -0600

    Setup Transactions Resource

---
+++ spec/models/transaction_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Transaction, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :credit_card_number }
+    it { should validate_presence_of :result }
+  end
+end
