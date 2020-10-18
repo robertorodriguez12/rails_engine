7fc28033165883706204b82a4e53e31fef4c6d1f
Hashim Gari <hashmaster3k@gmail.com>
2020-10-18 15:21:05 -0600

    Show endpoint - return specific merchant

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,5 +1,18 @@
 class Api::V1::MerchantsController < ApplicationController
   def index
-    render json: Merchant.all
+    merchants = { data: Merchant.select("id, name").map do |merchant|
+                        {id: merchant.id,
+                         type: 'merchant',
+                         attributes: {name: merchant.name}}
+                      end }
+
+    render json: merchants
+  end
+
+  def show
+    found = Merchant.find(params[:id])
+    merchant = { data: {id: found.id, type: 'found', attributes: {name: found.name}}}
+
+    render json: merchant
   end
 end
--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -8,7 +8,8 @@ RSpec.describe 'MERCHANT API' do
 
     expect(response).to be_successful
 
-    merchants = JSON.parse(response.body, symbolize_names: true)
+    json = JSON.parse(response.body, symbolize_names: true)
+    merchants = json[:data]
 
     expect(merchants).to be_an(Array)
     expect(merchants.count).to eq(5)
@@ -17,8 +18,37 @@ RSpec.describe 'MERCHANT API' do
       expect(merchant).to have_key(:id)
       expect(merchant[:id]).to be_an(Integer)
 
-      expect(merchant).to have_key(:name)
-      expect(merchant[:name]).to be_a(String)
+      expect(merchant).to have_key(:type)
+      expect(merchant[:type]).to be_an(String)
+
+      expect(merchant).to have_key(:attributes)
+      expect(merchant[:attributes]).to be_a(Hash)
+
+      expect(merchant[:attributes]).to have_key(:name)
+      expect(merchant[:attributes][:name]).to be_an(String)
     end
+  end
+
+  it 'can get one merchant by its id' do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    expect(response).to be_successful
+
+    json = JSON.parse(response.body, symbolize_names: true)
+    merchant = json[:data]
+
+    expect(merchant).to have_key(:id)
+    expect(merchant[:id]).to be_an(Integer)
+
+    expect(merchant).to have_key(:type)
+    expect(merchant[:type]).to be_an(String)
+
+    expect(merchant).to have_key(:attributes)
+    expect(merchant[:attributes]).to be_a(Hash)
+
+    expect(merchant[:attributes]).to have_key(:name)
+    expect(merchant[:attributes][:name]).to be_an(String)
   end
 end
