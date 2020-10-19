4a0923d7430fbca061c962dacbee320d95d1940d
Hashim Gari <hashmaster3k@gmail.com>
2020-10-19 16:00:05 -0600

    Show endpoint - return specific item

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -27,8 +27,34 @@ RSpec.describe 'ITEM API' do
       expect(item[:attributes]).to have_key(:name)
       expect(item[:attributes][:name]).to be_an(String)
       expect(item[:attributes][:description]).to be_an(String)
-      expect(item[:attributes][:unit_price]).to be_an(Integer)
+      expect(item[:attributes][:unit_price]).to be_an(Float)
       expect(item[:attributes][:merchant_id]).to be_an(Integer)
     end
+  end
+
+  it 'can get one item by its id' do
+    id = create(:item).id
+
+    get "/api/v1/items/#{id}"
+
+    expect(response).to be_successful
+
+    json = JSON.parse(response.body, symbolize_names: true)
+    item = json[:data]
+
+    expect(item).to have_key(:id)
+    expect(item[:id]).to be_an(String)
+
+    expect(item).to have_key(:type)
+    expect(item[:type]).to be_an(String)
+
+    expect(item).to have_key(:attributes)
+    expect(item[:attributes]).to be_a(Hash)
+
+    expect(item[:attributes]).to have_key(:name)
+    expect(item[:attributes][:name]).to be_an(String)
+    expect(item[:attributes][:description]).to be_an(String)
+    expect(item[:attributes][:unit_price]).to be_an(Float)
+    expect(item[:attributes][:merchant_id]).to be_an(Integer)
   end
 end
