f7cb6db3243f74bfc60bca1c9dc2f3b820e17a55
Hashim Gari <hashmaster3k@gmail.com>
2020-10-20 13:03:06 -0600

    SF - Item by unit_price

--- app/controllers/api/v1/items/search_controller.rb
+++ app/controllers/api/v1/items/search_controller.rb
@@ -4,6 +4,10 @@ class Api::V1::Items::SearchController < ApplicationController
       item = Item.find_by('description ILIKE ?', "%#{params[:description].downcase}%")
       return nil if item == nil
       render json: ItemSerializer.new(item)
+    elsif params[:unit_price]
+      item = Item.find_by(unit_price: params[:unit_price])
+      return nil if item == nil
+      render json: ItemSerializer.new(item)
     else #name
       item = Item.find_by('name ILIKE ?', "%#{params[:name].downcase}%")
       return nil if item == nil
--- spec/requests/api/v1/items/search_request_spec.rb
+++ spec/requests/api/v1/items/search_request_spec.rb
@@ -85,7 +85,7 @@ RSpec.describe 'ITEM API SEARCH' do
           expect(@json[:data][:attributes]).to have_key(:merchant_id)
           expect(@json[:data][:attributes][:merchant_id]).to be_a(Integer)
         end
-        
+
         it 'returns a single item by searching for full description' do
           get '/api/v1/items/find?description=MyString'
           expect(response).to be_successful
@@ -114,6 +114,44 @@ RSpec.describe 'ITEM API SEARCH' do
       describe 'sad' do
         it 'returns 204 if no match found' do
           get '/api/v1/items/find?description=blonde'
+          expect(response).to be_successful
+          expect(response.status).to eq(204)
+        end
+      end
+    end
+
+    describe 'item unit_price' do
+      before :each do
+        create_list(:item, 3)
+      end
+
+      describe 'happy' do
+        it 'returns a single item by searching by unit_price' do
+          get '/api/v1/items/find?unit_price=1.5'
+          expect(response).to be_successful
+          json = JSON.parse(response.body, symbolize_names: true)
+          expect(json).to have_key(:data)
+          expect(json[:data]).to be_a(Hash)
+          expect(json[:data]).to have_key(:id)
+          expect(json[:data][:id]).to_not be_empty
+          expect(json[:data]).to have_key(:type)
+          expect(json[:data][:type]).to_not be_empty
+          expect(json[:data]).to have_key(:attributes)
+          expect(json[:data][:attributes]).to be_a(Hash)
+          expect(json[:data][:attributes]).to have_key(:name)
+          expect(json[:data][:attributes][:name]).to be_a(String)
+          expect(json[:data][:attributes]).to have_key(:description)
+          expect(json[:data][:attributes][:description]).to be_a(String)
+          expect(json[:data][:attributes]).to have_key(:unit_price)
+          expect(json[:data][:attributes][:unit_price]).to be_a(Float)
+          expect(json[:data][:attributes]).to have_key(:merchant_id)
+          expect(json[:data][:attributes][:merchant_id]).to be_a(Integer)
+        end
+      end
+
+      describe 'sad' do
+        it 'returns 204 if no match found' do
+          get '/api/v1/items/find?unit_price=2.0'
           expect(response).to be_successful
           expect(response.status).to eq(204)
         end
