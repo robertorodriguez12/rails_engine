e5506e0e6f6f0cdc557cad379637c1c25261bcdb
Hashim Gari <hashmaster3k@gmail.com>
2020-10-20 11:31:02 -0600

    SF - Merchant by creation date

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,6 +1,14 @@
 class Api::V1::Merchants::SearchController < ApplicationController
   def show
-    merchant = Merchant.find_by('name ILIKE ?', "%#{params[:name].downcase}%")
-    render json: MerchantSerializer.new(merchant)
+    if params[:created_at]
+      date = Date.parse(params[:created_at])
+      merchant = Merchant.where(created_at: date.beginning_of_day..date.end_of_day).first
+      return nil if merchant == nil
+      render json: MerchantSerializer.new(merchant)
+    else
+      merchant = Merchant.find_by('name ILIKE ?', "%#{params[:name].downcase}%")
+      return nil if merchant == nil
+      render json: MerchantSerializer.new(merchant)
+    end
   end
 end
